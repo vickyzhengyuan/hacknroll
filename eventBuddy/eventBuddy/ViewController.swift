258da6555080b1998102de3ea5e5f320f72642c5
TangJiahui <tangjiahui0610@gmail.com>
2017-01-21 17:26:35 +0800

    initial commit

---
+++ eventBuddy/eventBuddy/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  eventBuddy
+//
+//  Created by Tang Jiahui on 21/1/17.
+//  Copyright © 2017 hacknroll2017. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var myTitle: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
