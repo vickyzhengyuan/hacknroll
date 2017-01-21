03a0c3cb1225d1592b165d23a8e5aae0d1811dde
TangJiahui <tangjiahui0610@gmail.com>
2017-01-21 18:50:45 +0800

    configure basic table content

--- eventBuddy/eventBuddy/ViewController.swift
+++ eventBuddy/eventBuddy/ViewController.swift
@@ -6,34 +6,16 @@
 //  Copyright © 2017 hacknroll2017. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            
     }
     
-    @IBOutlet weak var myTitle: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
