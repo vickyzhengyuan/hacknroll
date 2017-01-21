373f37d66e559e2cb13456c4696f2950f559ee1b
TangJiahui <tangjiahui0610@gmail.com>
2017-01-21 19:36:34 +0800

    configure cells & add navigation segue

---
+++ eventBuddy/eventBuddy/EventTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EventTableViewController.swift
+//  eventBuddy
+//
+//  Created by Tang Jiahui on 21/1/17.
+//  Copyright © 2017 hacknroll2017. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
